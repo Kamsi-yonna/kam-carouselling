e9be360bcd5adc7d8571029bc331c10ff2f0a138
Kamsi-yonna <etiobikamsi@gmail.com>
2024-11-21 20:18:28 +0100

    feat - added APT project page and hero image

---
+++ pages/projects/APT-Dolomiti-Paganella.vue
@@ -0,0 +1,165 @@
+<template>
+    <div>
+        <div class="letter-hero_section">
+            <div class="letter-hero_wrapper">
+                <div class="letter-hero_details">
+                    <div class="title_block">
+                        <div class="blue-text_span">APT Dolomiti Paganella</div>
+                    </div>
+                    <div class="letter-share_wrapper">
+                        <div>share</div>
+                        <div class="social-links_block">
+                            <a href="https://kam-caroselling.webflow.io/projects/apt-dolomiti-paganella#"
+                                class="social-link w-inline-block"><img
+                                    src="https://cdn.prod.website-files.com/67245aca623c6cfc8f245f78/6732e5fbd12ebee771e7ceca_instagram.svg"
+                                    loading="lazy" alt="" class="cc-image c-contain" /></a><a
+                                href="https://kam-caroselling.webflow.io/projects/apt-dolomiti-paganella#"
+                                class="social-link w-inline-block"><img
+                                    src="https://cdn.prod.website-files.com/67245aca623c6cfc8f245f78/6732e5fb1ecc3b35a9812ee7_twitter.svg"
+                                    loading="lazy" alt="" class="cc-image c-contain" /></a><a
+                                href="https://kam-caroselling.webflow.io/projects/apt-dolomiti-paganella#"
+                                class="social-link w-inline-block"><img
+                                    src="https://cdn.prod.website-files.com/67245aca623c6cfc8f245f78/6732e5fb7060f1754ebab753_facebook.svg"
+                                    loading="lazy" alt="" class="cc-image c-contain" /></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="hero-image_wrapper">
+                    <div class="hero-image_block">
+                        <img src="public/assets/images/projects/673a508e830b9cdde51c960d_paganella-1_ktpybg.webp"
+                            loading="lazy" alt="" class="hero-image" />
+                    </div>
+                    <div class="blue-image_bg"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="project-content_section">
+            <div class="project-content_wrapper">
+                <div id="w-node-_37738701-958c-6f6e-0883-418cca62c8b3-477c936f" class="project-date_block"></div>
+                <div class="project-content_block">
+                    <div class="header_content w-condition-invisible">
+                        <div class="w-dyn-bind-empty w-richtext"></div>
+                    </div>
+                    <div class="article-block">
+                        <div class="article-header">
+                            <div>Customer</div>
+                            <div class="cc-underline quarter-width"></div>
+                        </div>
+                        <div class="header_content">
+                            <div class="w-richtext">
+                                <p>
+                                    In the tourism sector, the Dolomiti Paganella Tourist Board
+                                    is responsible for promoting holidays and experiences in all
+                                    seasons, in Trentino, on the Paganella Plateau.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="article-image_block">
+                            <img src="./APT Dolomiti Paganella_files/placeholder.60f9b1840c.svg" loading="lazy" alt=""
+                                class="cc-image w-dyn-bind-empty" />
+                        </div>
+                    </div>
+                    <div class="article-block">
+                        <div class="article-header">
+                            <div>What we did</div>
+                            <div class="cc-underline quarter-width"></div>
+                        </div>
+                        <div class="header_content">
+                            <div class="w-richtext">
+                                <p>
+                                    The amount of texts was varied and substantial, almost 120
+                                    texts ranging from the territory, to experiences, to tourist
+                                    places, to cultural itineraries, to all the countless sports
+                                    that can be practiced on the Paganella both in summer and
+                                    winter.
+                                </p>
+                                <p>
+                                    We took care of the research, copywriting (both creative and
+                                    informative), menus and submenus, naming of some services
+                                    (the game), adapting the target audience, the type of
+                                    information, the appeal or precision, creativity and
+                                    meticulousness to each type of text.
+                                </p>
+                                <p>
+                                    It was a long and important project and it was worth it!
+                                </p>
+                            </div>
+                        </div>
+                        <div class="article-image_block">
+                            <img src="./APT Dolomiti Paganella_files/placeholder.60f9b1840c.svg" loading="lazy" alt=""
+                                class="cc-image w-dyn-bind-empty" />
+                        </div>
+                    </div>
+                    <div class="article-block w-condition-invisible">
+                        <div class="article-header">
+                            <div class="w-dyn-bind-empty"></div>
+                            <div class="cc-underline quarter-width"></div>
+                        </div>
+                        <div class="header_content">
+                            <div class="w-dyn-bind-empty w-richtext"></div>
+                        </div>
+                        <div class="article-image_block">
+                            <img src="./APT Dolomiti Paganella_files/placeholder.60f9b1840c.svg" loading="lazy" alt=""
+                                class="cc-image w-dyn-bind-empty" />
+                        </div>
+                    </div>
+                    <div class="article-block w-condition-invisible">
+                        <div class="article-header">
+                            <div class="w-dyn-bind-empty"></div>
+                            <div class="cc-underline quarter-width"></div>
+                        </div>
+                        <div class="header_content">
+                            <div class="w-dyn-bind-empty w-richtext"></div>
+                        </div>
+                        <div class="article-image_block">
+                            <img src="./APT Dolomiti Paganella_files/placeholder.60f9b1840c.svg" loading="lazy" alt=""
+                                class="cc-image w-dyn-bind-empty" />
+                        </div>
+                        <div class="article-image_block">
+                            <img src="./APT Dolomiti Paganella_files/placeholder.60f9b1840c.svg" loading="lazy" alt=""
+                                class="cc-image w-dyn-bind-empty" />
+                        </div>
+                        <div class="article-image_block">
+                            <img src="./APT Dolomiti Paganella_files/placeholder.60f9b1840c.svg" loading="lazy" alt=""
+                                class="cc-image w-dyn-bind-empty" />
+                        </div>
+                    </div>
+                    <div class="article-block w-condition-invisible">
+                        <div class="article-header">
+                            <div class="w-dyn-bind-empty"></div>
+                            <div class="cc-underline quarter-width"></div>
+                        </div>
+                        <div class="header_content">
+                            <div class="w-dyn-bind-empty w-richtext"></div>
+                        </div>
+                        <div class="article-image_block">
+                            <img src="/public/assets/images/projects/673a508e830b9cdde51c960d_paganella-1_ktpybg.webp"
+                                loading="lazy" alt="" class="cc-image w-dyn-bind-empty" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const metaTags = useDefault('meta')
+const route = useRoute()
+const metaTitle = route.path.split('/').pop()!.replace('.vue', '');
+
+useSeoMeta({
+    ...metaTags,
+    title: `${metaTitle} | Projects`,
+    ogTitle: `${metaTitle} | Projects`,
+    description:
+        "Customer",
+    ogDescription:
+        "Customer",
+    ogImage:
+        "https://cdn.prod.website-files.com/6732d301546e113668feb3eb/673a508e830b9cdde51c960d_paganella-1_ktpybg.webp",
+})
+</script>
+
+<style scoped></style>
