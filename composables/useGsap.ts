199b92c1a8397279dfb072e4f6921f8a05ec151f
Kamsi-yonna <etiobikamsi@gmail.com>
2024-12-04 07:31:48 +0100

    feat - added a fade in animation for hero sections and navbar

--- composables/useGsap.ts
+++ composables/useGsap.ts
@@ -8,8 +8,39 @@ gsap.registerPlugin(ScrollTrigger);
 export default function useGsap() {
   const fadeInAnimation = () => {
     const fadeInElements = document.querySelectorAll("[data-fade-anim]");
+    const heroFadeInAnimation = document.querySelectorAll("[hero-fade-anim]");
+    const articleFadeInAnimation = document.querySelectorAll(".article-block");
 
     fadeInElements.forEach((element) => {
+      const anim = gsap.fromTo(
+        element,
+        { opacity: 0, y: 20, x: 100 },
+        {
+          opacity: 1,
+          y: 0,
+          x: 0,
+          duration: 0.8,
+          ease: "power1.inOut",
+          paused: true,
+        }
+      );
+
+      // ScrollTrigger for playing the animation
+      ScrollTrigger.create({
+        trigger: element,
+        start: "top 70%",
+        onEnter: () => anim.play(),
+      });
+
+      // ScrollTrigger for resetting the animation
+      ScrollTrigger.create({
+        trigger: element,
+        start: "top bottom",
+        onLeaveBack: () => anim.pause(0),
+      });
+    });
+
+    articleFadeInAnimation.forEach((element) => {
       gsap.fromTo(
         element,
         { opacity: 0, y: 20, x: 100 },
@@ -25,6 +56,19 @@ export default function useGsap() {
             toggleActions: "play none none none",
             markers: false,
           },
+        }
+      );
+    });
+
+    heroFadeInAnimation.forEach((element) => {
+      gsap.fromTo(
+        element,
+        { opacity: 0, y: 30 },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 0.8,
+          ease: "power1.inOut",
         }
       );
     });
